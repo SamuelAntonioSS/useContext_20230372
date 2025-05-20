f8f40d5502f18a5f62432481df86d3071feb426a
Samu06 <20230372@ricaldone.edu.sv>
2025-05-20 09:07:44 -0600

    Se realizo la primera parte

---
+++ use_Context_20230372/src/page/Dashboard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Dashboard.css";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+// Dashboard.jsx
+
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const { user, logOut } = useAuth();
+
+  const handleLogout = () => {
+    if (logOut()) {
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-card">
+        <h1 className="dashboard-title">¡Bienvenido al Dashboard!</h1>
+        <p className="dashboard-user">
+          Hola, <span>{user || "Usuario logueado"}</span>
+        </p>
+        <button className="dashboard-button" onClick={handleLogout}>
+          Cerrar sesión
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
